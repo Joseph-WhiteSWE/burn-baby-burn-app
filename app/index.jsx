e512efff65acf9cf3c318a4013f3ba28fb5a5b99
joseph <white.joseph.jr@gmail.com>
2023-12-01 18:13:05 -0600

    designed welcome screen

--- app/index.jsx
+++ app/index.jsx
@@ -1,10 +1,43 @@
-import { View, Text } from 'react-native'
+import { View, Text, Image, TouchableOpacity } from 'react-native'
 import React from 'react'
+import {widthPercentageToDP as wp, heightPercentageToDP as hp} from 'react-native-responsive-screen'
+import { StatusBar } from 'expo-status-bar'
+import { LinearGradient } from 'expo-linear-gradient'
 
 export default function Index() {
   return (
     <View className="flex-1 justify-center items-center">
-      <Text>first screen</Text>
+     <StatusBar style="light"/>
+     <Image className="h-full w-full absolute" source={require('../assets/images/welcome.png')} />
+
+    <LinearGradient 
+    colors={['transparent', '#18181b']}
+    style={{width: wp(100), height: hp(70)}}
+    start={{x:0.5, y:0}}
+    end={{x:0.5, y:0.8}}
+    className="flex justify-end pb-12 space-y-8" 
+    >
+      <View className="flex items-center ">
+        <Text style={{fontSize: hp(6)}} className="text-white font-bold tracking-wide">
+          Best 
+          <Text className="text-red-500"> Workouts</Text>
+        </Text>
+        <Text style={{fontSize: hp(5)}} className="text-white font-bold tracking-wide">
+          For you
+        </Text>
+      </View>
+      <View>
+        <TouchableOpacity
+        style={{height: hp(7), width: wp(80)}}
+        className="bg-red-500 flex items-center justify-center mx-auto rounded-full border-[2px] border-neutral-200"
+        >
+          <Text style={{fontSize: hp(3)}} className="text-white font-bold tracking-widest"> Let's Get Started</Text>
+
+        </TouchableOpacity>
+
+      </View>
+    </LinearGradient>
+    
     </View>
   )
 }
