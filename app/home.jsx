2d366f9c3bdb83542494f6f22f7f31eba3309f2b
joseph <white.joseph.jr@gmail.com>
2023-12-02 13:19:48 -0600

    finished punchline and avatar

--- app/home.jsx
+++ app/home.jsx
@@ -1,10 +1,49 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, Image } from "react-native";
+import React from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { StatusBar } from "expo-status-bar";
+import {
+  widthPercentageToDP as wp,
+  heightPercentageToDP as hp,
+} from "react-native-responsive-screen";
+import Ionicons from "react-native-vector-icons/Ionicons";
 
 export default function Home() {
   return (
-    <View className="flex-1 items-center justify-center">
-      <Text>Home</Text>
-    </View>
-  )
+    <SafeAreaView className="flex-1 bg-white flex space-y-5 " edges={["top"]}>
+      <StatusBar style="dark" />
+
+      {/* punchline and avatar */}
+      <View className="flex-row items-center justify-between mx-3">
+        <View className="space-y-2">
+          <Text
+            style={{ fontSize: hp(4.5) }}
+            className="font-bold tracking-wider text-neutral-700"
+          >
+            READY TO
+          </Text>
+          <Text
+            style={{ fontSize: hp(4.5) }}
+            className="font-bold tracking-wider text-red-600"
+          >
+            WORKOUT
+          </Text>
+        </View>
+
+        <View className="flex items-center justify-center space-y-2 ">
+          <Image
+            source={require("../assets/images/avatar.png")}
+            style={{ width: wp(12), height: hp(6) }}
+            className="rounded-full"
+          />
+          <View
+            className="flex bg-neutral-200 rounded-full items-center justify-center"
+            style={{ width: wp(11), height: hp(5) }}
+          >
+            <Ionicons name="notifications" size={hp(3)} color="gray" />
+          </View>
+        </View>
+      </View>
+    </SafeAreaView>
+  );
 }
--- app/index.jsx
+++ app/index.jsx
@@ -1,50 +1,71 @@
-import { View, Text, Image, TouchableOpacity } from 'react-native'
-import React from 'react'
-import {widthPercentageToDP as wp, heightPercentageToDP as hp} from 'react-native-responsive-screen'
-import { StatusBar } from 'expo-status-bar'
-import { LinearGradient } from 'expo-linear-gradient'
-import Animated, {FadeIn, FadeOut, FadeInDown} from 'react-native-reanimated'
-import { useRouter } from 'expo-router'
-
+import { View, Text, Image, TouchableOpacity } from "react-native";
+import React from "react";
+import {
+  widthPercentageToDP as wp,
+  heightPercentageToDP as hp,
+} from "react-native-responsive-screen";
+import { StatusBar } from "expo-status-bar";
+import { LinearGradient } from "expo-linear-gradient";
+import Animated, { FadeIn, FadeOut, FadeInDown } from "react-native-reanimated";
+import { useRouter } from "expo-router";
 
 export default function Index() {
-  const router = useRouter()
+  const router = useRouter();
   return (
+    // extends the view to full screen
     <View className="flex-1 justify-end">
-     <StatusBar style="light"/>
-     <Image className="h-full w-full absolute" source={require('../assets/images/welcome.png')} />
+      <StatusBar style="light" />
 
-    <LinearGradient 
-    colors={['transparent', '#18181b']}
-    style={{width: wp(100), height: hp(70)}}
-    start={{x: 0.5, y: 0}}
-    end={{x: 0.5, y: 0.8}}
-    className="flex justify-end pb-12 space-y-8" 
-    >
-      <Animated.View entering={FadeInDown.delay(100).springify()} className="flex items-center ">
-        <Text style={{fontSize: hp(6)}} className="text-white font-bold tracking-wide">
-          Best 
-          <Text className="text-red-500"> Workouts</Text>
-        </Text>
-        <Text style={{fontSize: hp(5)}} className="text-white font-bold tracking-wide">
-          For you
-        </Text>
-      </Animated.View >
+      {/* makes image full screen */}
+      <Image
+        className="h-full w-full absolute"
+        source={require("../assets/images/welcome.png")}
+      />
 
-      <Animated.View entering={FadeInDown.delay(200).springify()}>
-        <TouchableOpacity
-        //push to navigate to home screen
-        onPress={()=> router.push('home')}
-        style={{height: hp(7), width: wp(80)}}
-        className="bg-red-500 flex items-center justify-center mx-auto rounded-full border-[2px] border-neutral-200"
+      {/* adds a gradient */}
+      <LinearGradient
+        colors={["transparent", "#8b0000"]}
+        style={{ width: wp(100), height: hp(70) }}
+        start={{ x: 0.5, y: 0 }}
+        end={{ x: 0.5, y: 1 }}
+        className="flex justify-end pb-12 space-y-8"
+      >
+        <Animated.View
+          entering={FadeInDown.delay(100).springify()}
+          className="flex items-center "
         >
-          <Text style={{fontSize: hp(3)}} className="text-white font-bold tracking-widest"> Let's Get Started</Text>
+          <Text
+            style={{ fontSize: hp(6) }}
+            className="text-white font-bold tracking-wide"
+          >
+            Best
+            <Text className="text-red-600"> Workouts</Text>
+          </Text>
+          <Text
+            style={{ fontSize: hp(5) }}
+            className="text-white font-bold tracking-wide"
+          >
+            For you
+          </Text>
+        </Animated.View>
 
-        </TouchableOpacity>
-
-      </Animated.View>
-    </LinearGradient>
-    
+        <Animated.View entering={FadeInDown.delay(400).springify()}>
+          {/* make it into a touchable component */}
+          <TouchableOpacity
+            //use expo router to navigate components
+            onPress={() => router.push("home")}
+            style={{ height: hp(7), width: wp(80) }}
+            className="bg-red-600 flex items-center justify-center mx-auto rounded-full border-[2px] border-neutral-200"
+          >
+            <Text
+              style={{ fontSize: hp(3) }}
+              className="text-white font-bold tracking-widest"
+            >
+              Let's Get Started
+            </Text>
+          </TouchableOpacity>
+        </Animated.View>
+      </LinearGradient>
     </View>
-  )
+  );
 }
